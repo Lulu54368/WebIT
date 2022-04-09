940ec95a9cbac6625a80e892eed5f32b0f7c0c92
yuntaol <yuntaol@student.unimelb.edu.au>
2022-04-09 12:24:58 +1000

    include clinician id in router

--- controller/clinicianController.js
+++ controller/clinicianController.js
@@ -1,13 +1,24 @@
-const patients_medical_data = require("../models/utils/patient_medical_data");
+const patients_medical_list = require("../models/utils/patient_medical_data");
 const patients_data = require("../models/patient_data");
+const clinician_data = require("../models/clinician.js")
 const getAllPatients = (req, res)=>{
-    //res.render("../HistoryData/historyData.hbs");
+    
+    const clinician = clinician_data.find((one)=>one.id == req.params.id);
+
+    const patient_id_list = clinician.patients;
+    
+    const patients_medical_data = patients_medical_list.filter((patient)=>
+    patient_id_list.includes(patient.id)
+    )
+    
     res.render("../views/layouts/clinician_dashboard.hbs",{patients: patients_medical_data});
     
 }
 const getOnePatient = (req, res)=>{
-    const patient = patients_data.find((one)=> one.id == req.params.id);
-    if(patient){
+    const clinician = clinician_data.find((one)=>one.id == req.params.id);
+    const patient_id_list = clinician.patients;
+    const patient = patients_data.find((one)=> one.id == req.params.patient_id);
+    if(patient && patient_id_list.includes(patient.id)){
         res.render("../views/layouts/clinician_patientdata.hbs",{name: patient.name, 
             patient_data: patient.data});
     }
