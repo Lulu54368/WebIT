2343ccfe994aba9f3a482ec01dd89c2e5fec58f4
yuntaol <yuntaol@student.unimelb.edu.au>
2022-04-13 20:05:48 +1000

    add task distribution

--- router/clinician.js
+++ router/clinician.js
@@ -1,10 +1,25 @@
 const express = require("express");
 const clinicianRouter = express.Router();
-const clinicianController = require("../controller/clinicianController")
+const clinicianController = require("../controller/clinicianController");
+
 //Get a patient's data of a specified clinician
 clinicianRouter.get("/:id/:patient_id", clinicianController.getOnePatient);
 //Get all patients' data of a specified clinician
 clinicianRouter.get("/:id", clinicianController.getAllPatients);
 //Change whatever the patient need to input
 clinicianRouter.post("/:id/:patient_id", clinicianController.changeInput);
+
+/*
+Phillips
+//get all of patient's comment (id is clinincian id)
+clinicianRouter.get("/:id/comments", clinicianController.getAllComment);
+//get all of threshold 
+clinicianRouter.get("/:id/threshold", clinicianController.getAllThreshold);
+//change threshold of a specified patient
+clinicianRouter.post("/:id/:patient_id/threshold", clinicianController.modifyThreshold);
+//get all of support sentence
+clinicianRouter.get("/:id/support", clinicianController.getSupportSentence)
+//add support sentence
+clinicianRouter.post("/:id/:patient_id/support", clinicianController.addSupportSentence);
+*/
 module.exports = clinicianRouter;
--- router/patient.js
+++ router/patient.js
@@ -2,9 +2,11 @@ const express = require("express");
 const patientController = require("../controller/patientController.js");
 const patientRouter = express.Router();
 //get current patient's data
-patientRouter.get("/:id", patientController.getCurrData);
+patientRouter.get("/:id/data", patientController.getCurrData);
+
+
+
 
 //add current patient's data
 patientRouter.post("/:id", patientController.addTodayData);
-
 module.exports = patientRouter;
