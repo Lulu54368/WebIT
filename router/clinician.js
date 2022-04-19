59f9be630e9d0c6a058db531e953f297a9c89acc
yuntaol <yuntaol@student.unimelb.edu.au>
2022-04-19 19:12:09 +1000

    clinician_router

---
+++ router/clinician.js
@@ -0,0 +1,34 @@
+
+const express = require("express");
+const clinicianRouter = express.Router();
+const clinicianController = require("../controller/clinicianController");
+
+
+//have to replace every id here with clinician_id
+//Get a patient's data of a specified clinician
+clinicianRouter.get("/:clinician_id/:patient_id", clinicianController.getOnePatient);
+//Get all patients' data of a specified clinician
+clinicianRouter.get("/:clinician_id", clinicianController.getAllPatients);
+//Change whatever the patient need to input
+clinicianRouter.post("/:id/:patient_id", clinicianController.changeInput);
+
+//add a patient
+clinicianRouter.post("/:clinician_id", clinicianController.addOnePatient);
+
+//Phillips
+//get all of patient's comment (id is clinincian id)
+clinicianRouter.get("/:id//comments", clinicianController.getAllComments);
+
+//get all of threshold 
+clinicianRouter.get("/:id//threshold", clinicianController.getAllThreshold);
+
+//change threshold of a specified patient
+clinicianRouter.post("/:id/:patient_id/threshold", clinicianController.modifyThreshold);
+
+//get all of support sentence
+clinicianRouter.get("/:id//support", clinicianController.getSupportSentence)
+
+//add support sentence
+clinicianRouter.post("/:id/:patient_id/support", clinicianController.addSupportSentence);
+
+module.exports = clinicianRouter;
