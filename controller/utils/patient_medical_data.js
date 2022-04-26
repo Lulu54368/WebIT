d7ba28d99940170b445d2f64597cd386101eb258
anzhec <anzhe.cai@student.unimelb.edu.au>
2022-04-26 23:18:50 +1000

    merged patient data and threshold data, assuming each patient would definitely have a threshold when being added to database, and highlight is displayable now

--- controller/utils/patient_medical_data.js
+++ controller/utils/patient_medical_data.js
@@ -26,8 +26,25 @@ const patient_data_list=(patient_threshold_data, patient_data)=>{
             component["exercise_lb"] = patient_threshold_data[i].threshold.exercise.lower_bound
             component["insulin_ub"] = patient_threshold_data[i].threshold.insulin_intake.upper_bound
             component["insulin_lb"] = patient_threshold_data[i].threshold.insulin_intake.lower_bound
-            // All upper/lower bounds would have been passed in a request body or set as default values  
+            component["blood_highlight"] = false;
+            component["weight_highlight"] = false;
+            component["exercise_highlight"] = false;
+            component["insulin_highlight"] = false;
+            // All upper/lower bounds would have been passed in a request body or set as default values, once a patient is added to database 
         })
+        if ((component.blood_level < component.blood_lb) || component.blood_level > component.blood_ub) {
+            component["blood_highlight"] = true;
+        }
+        if ((component.weight < component.weight_lb) || component.weight > component.weight_ub) {
+            component["weight_highlight"] = true;
+        }
+        if ((component.exercise < component.exercise_lb) || component.exercise > component.exercise_ub) {
+            console.log("utils/patient_medical data line 42, i = " + i);
+            component["exercise_highlight"] = true;
+        }
+        if ((component.insulin_intake < component.insulin_lb) || component.insulin_intake > component.insulin_ub) {
+            component["insulin_highlight"] = true;
+        }
         patient_medical_list.push(component);
         i++;
     });
