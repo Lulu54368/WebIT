00ae5aa8f8e0e821bad4df4ccda1811061c05425
anzhec <anzhe.cai@student.unimelb.edu.au>
2022-05-06 15:48:48 +1000

    adjust router order

--- router/clinician.js
+++ router/clinician.js
@@ -3,33 +3,32 @@ const express = require("express");
 const clinicianRouter = express.Router();
 const clinicianController = require("../controller/clinicianController");
 
-
-//have to replace every id here with clinician_id
-//Get one patient's data of a specified clinician
-clinicianRouter.get("/:clinician_id/:patient_id", clinicianController.getOnePatient);
 //Get all patients' data of a specified clinician
 clinicianRouter.get("/:clinician_id", clinicianController.getAllPatients);
+
+//add a patient
+clinicianRouter.post("/:clinician_id", clinicianController.addOnePatient);
+
+//get all of patient's comment (id is clinincian id)
+clinicianRouter.get("/:clinician_id/comments", clinicianController.getAllComments);
+
+//get all of threshold 
+clinicianRouter.get("/:clinician_id/threshold", clinicianController.getAllThreshold);
+
+//get all of support sentence
+clinicianRouter.get("/:clinician_id/support", clinicianController.getSupportSentence)
+
+//Get one patient's data of a specified clinician
+clinicianRouter.get("/:clinician_id/:patient_id", clinicianController.getOnePatient);
+
 //Add fields the patient need to input
 clinicianRouter.post("/:clinician_id/:patient_id/add", clinicianController.addInput);
 
 //Delete fields the patient doesn't need to input
 clinicianRouter.post("/:clinician_id/:patient_id/delete", clinicianController.deleteInput);
 
-//add a patient
-clinicianRouter.post("/:clinician_id", clinicianController.addOnePatient);
-
-//Phillips
-//get all of patient's comment (id is clinincian id)
-clinicianRouter.get("/:clinician_id//comments", clinicianController.getAllComments);
-
-//get all of threshold 
-clinicianRouter.get("/:clinician_id//threshold", clinicianController.getAllThreshold);
-
 //change threshold of a specified patient
 clinicianRouter.post("/:clinician_id/:patient_id/threshold", clinicianController.modifyThreshold);
-
-//get all of support sentence
-clinicianRouter.get("/:clinician_id//support", clinicianController.getSupportSentence)
 
 //add support sentence
 clinicianRouter.post("/:clinician_id/:patient_id/support", clinicianController.addSupportSentence);
