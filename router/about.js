9f2f728cac294f18aef41705eb46f45f3c913fca
juicyka <qzzhu1@student.unimelb.edu.au>
2022-05-11 22:28:51 +1000

    rename files

--- router/about.js
+++ router/about.js
@@ -2,11 +2,11 @@ const express = require("express");
 const aboutRouter = express.Router();
 
 aboutRouter.get("/about_diabetes", (req, res) => {
-  res.render("../views/about/aboutdiabetes.hbs");
+  res.render("../views/about/about_diabetes.hbs");
 });
 
 aboutRouter.get("/about_website", (req, res) => {
-  res.render("../views/about/aboutthiswebsite.hbs");
+  res.render("../views/about/about_website.hbs");
 });
 
 aboutRouter.get("/login", (req, res) => {
