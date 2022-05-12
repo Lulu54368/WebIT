44fb21bfe2ba352b8d051b39d2a1f41e49a23c93
juicyka <qzzhu1@student.unimelb.edu.au>
2022-05-12 22:11:36 +1000

    render change pwd

--- controller/patientController.js
+++ controller/patientController.js
@@ -8,175 +8,181 @@ const { deleteOne } = require("../models/patient_input");
 const Patient = Patients.Patients; //patient model
 const Patient_Data_Schema = Patients.patient_data; //schema
 const Data_Schema = Patients.Data;
-const alert = require("alert")
+const alert = require("alert");
 
-const getCurrData = async (req, res)=>{
-    try {
-        var attributes = await Patient_input.findOne({id: req.params.patient_id.toString()}).lean();
-        attributes = attributes.input;
-        const patient = await Patient.findById(req.params.patient_id);
-        const today = new Date().toLocaleDateString();
-        const all_input = ["blood_level", "weight", "insulin_intake", "exercise"];
-        var today_data = patient.data.find(
-            (one)=>(one.date == today)
-        );
-    
-        // If today's data is not available, manipulate an empty object
-        if(!today_data){
-            today_data = {};
-            attributes.forEach((attr)=>
-            {
-                today_data[attr] = {
-                data: "",
-                comment: "",
-                recorded: false
-                };
-                today_data[attr].required = true; //patient required to enter
-            });
-            
-        }
-     
-        all_input.filter((input)=>!attributes.includes(input))
-        .forEach((input)=>{
-            today_data[input] = {required: false}
-        });
-       console.log({data: today_data , patient_name:patient.name, today: new Date().toLocaleDateString(), patient_input: attributes, message: patient.message})
-        //only show the data of the attribute patient required to input 
-        res.render("../views/layouts/patienthomepage.hbs",
-        {data: today_data , patient_name:patient.name, today: new Date().toLocaleDateString(), patient_input: attributes, message: patient.message});
-    
-    }
-    catch(err) {
-        console.log(err);
-    }
-    
-}
+const getCurrData = async (req, res) => {
+  try {
+    var attributes = await Patient_input.findOne({
+      id: req.params.patient_id.toString(),
+    }).lean();
+    attributes = attributes.input;
+    const patient = await Patient.findById(req.params.patient_id);
+    const today = new Date().toLocaleDateString();
+    const all_input = ["blood_level", "weight", "insulin_intake", "exercise"];
+    var today_data = patient.data.find((one) => one.date == today);
 
+    // If today's data is not available, manipulate an empty object
+    if (!today_data) {
+      today_data = {};
+      attributes.forEach((attr) => {
+        today_data[attr] = {
+          data: "",
+          comment: "",
+          recorded: false,
+        };
+        today_data[attr].required = true; //patient required to enter
+      });
+    }
+
+    all_input
+      .filter((input) => !attributes.includes(input))
+      .forEach((input) => {
+        today_data[input] = { required: false };
+      });
+    console.log({
+      data: today_data,
+      patient_name: patient.name,
+      today: new Date().toLocaleDateString(),
+      patient_input: attributes,
+      message: patient.message,
+    });
+    //only show the data of the attribute patient required to input
+    res.render("../views/layouts/patienthomepage.hbs", {
+      data: today_data,
+      patient_name: patient.name,
+      today: new Date().toLocaleDateString(),
+      patient_input: attributes,
+      message: patient.message,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 //add a piece of data
-const addOneData = async (req, res)=>{
-    try{
-        var attributes = await Patient_input.findOne({id: req.params.patient_id}).lean();
+const addOneData = async (req, res) => {
+  try {
+    var attributes = await Patient_input.findOne({
+      id: req.params.patient_id,
+    }).lean();
     attributes = attributes.input;
 
     patient = await Patient.findById(req.params.patient_id);
     const newData = req.body;
-    if(JSON.stringify(newData) != "{}"){
-        // find today's data
-        const key_attr = req.body.key;
-        var data = patient.data.find((data)=>data.date == new Date().toLocaleDateString());
- 
-        if(!data){
-            
-            data = {};
-            data.date = new Date().toLocaleDateString();  //timeStamp
-            attributes.forEach((attr)=>{
-                
-                data[attr] = {
-                data: "",
-                comment: "",
-                recorded: false,
-                required: true
-            }
-            
-        });    //initialize
-            
-            data[key_attr].data = req.body.value;
-            data[key_attr].comment = req.body.comment;
-            data[key_attr].recorded = true; //record data
-            data[key_attr].createAt = new Date().toLocaleDateString();
-            patient.data.push(data) //push data
+    if (JSON.stringify(newData) != "{}") {
+      // find today's data
+      const key_attr = req.body.key;
+      var data = patient.data.find(
+        (data) => data.date == new Date().toLocaleDateString()
+      );
+
+      if (!data) {
+        data = {};
+        data.date = new Date().toLocaleDateString(); //timeStamp
+        attributes.forEach((attr) => {
+          data[attr] = {
+            data: "",
+            comment: "",
+            recorded: false,
+            required: true,
+          };
+        }); //initialize
+
+        data[key_attr].data = req.body.value;
+        data[key_attr].comment = req.body.comment;
+        data[key_attr].recorded = true; //record data
+        data[key_attr].createAt = new Date().toLocaleDateString();
+        patient.data.push(data); //push data
+      } else {
+        var attr_data = patient.data.find(
+          (data) => data[key_attr].createAt == new Date().toLocaleDateString()
+        );
+        if (!attr_data) {
+          data[key_attr].data = req.body.value;
+          data[key_attr].comment = req.body.comment;
+          data[key_attr].recorded = true;
+          data[key_attr].createAt = new Date().toLocaleDateString();
         }
-        else{
-            var attr_data = patient.data.find((data)=>data[key_attr].createAt == new Date().toLocaleDateString());
-            if(!attr_data){
-                data[key_attr].data = req.body.value;
-                data[key_attr].comment = req.body.comment;
-                data[key_attr].recorded = true;
-                data[key_attr].createAt = new Date().toLocaleDateString();
-            }
-            patient.data.pop()//remove the latest data
-            patient.data.push(data);          
-        }
-        
-        await patient.save();
-       
-        res.redirect("/patient/"+ req.params.patient_id);
-            
+        patient.data.pop(); //remove the latest data
+        patient.data.push(data);
+      }
+
+      await patient.save();
+
+      res.redirect("/patient/" + req.params.patient_id);
+    } else {
+      res.send("no patient sent");
     }
-    else{
-        res.send("no patient sent");
-    }
-    }
-    catch(err){
-        console.log(err);
-    }
-  
-}
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 //This function get the medical data for a specified patient
-const getPatientHistory = async (req, res)=>{
-    try{
-        const today = new Date().toLocaleDateString();
-        const patient = await Patient.findById(req.params.patient_id).lean();
-        console.log("patientController line 114");
-        if(patient){
-            res.render("../views/layouts/patient_historyData.hbs",{today: new Date().toLocaleDateString(), patient_name: patient.name, 
-                history: patient.data});
-        }
-        else{
-            res.send("patient not found");
-        }
+const getPatientHistory = async (req, res) => {
+  try {
+    const today = new Date().toLocaleDateString();
+    const patient = await Patient.findById(req.params.patient_id).lean();
+    console.log("patientController line 114");
+    if (patient) {
+      res.render("../views/layouts/patient_historyData.hbs", {
+        today: new Date().toLocaleDateString(),
+        patient_name: patient.name,
+        history: patient.data,
+      });
+    } else {
+      res.send("patient not found");
     }
-    catch(err){
-        console.log(err);
-    }
-    
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-}
+const renderLogin = () => {
+  res.render("../views/layouts/login.hbs");
+};
+const logout = (req, res) => {
+  req.logout();
+  res.redirect("patient/login");
+};
 
+const renderChangePwd = (req, res) => {
+  res.render("change_pwd.hbs");
+};
 
+const changePassword = async (req, res) => {
+  // req body is {currPassWord, newPassword}
+  try {
+    const patient = await Patient.findById(req.params.patient_id);
+    if (patient == null) {
+      res.send("can not find patient");
+    } else {
+      console.log(req.body);
 
-const renderLogin = ()=>{
-    
-    res.render("../views/layouts/login.hbs");
-}
-const logout = (req, res) =>{
-    req.logout();
-    res.redirect("patient/login");
-}
-const changePassword = async (req, res) =>{
-    // req body is {currPassWord, newPassword}
-    try{
-        const patient = await Patient.findById(req.params.patient_id);
-        if (patient == null){
-            res.send("can not find patient");
+      await patient.verifyPassword(req.body.currPassword, (err, valid) => {
+        if (valid == true) {
+          patient.password = req.body.newPassword;
+          patient.save();
+          //res.send("saved!");
+          alert("saved !");
+          res.redirect("/patient/" + req.params.patient_id);
+        } else {
+          //res.send("password not match");
+          alert("something went wrong...");
+          res.redirect("/patient/" + req.params.patient_id);
         }
-        else{
-            console.log(req.body);
-            await patient.verifyPassword(req.body.currPassword, (err, valid)=>{
-                if(valid == true){
-                    patient.password = req.body.newPassword;
-                    patient.save();
-                    //res.send("saved!");
-                    alert("saved !")
-                    res.redirect("/patient/"+req.params.patient_id);
-                }
-                else{
-                  
-                    //res.send("password not match");
-                    alert("something went wrong...")
-                    res.redirect("/patient/"+req.params.patient_id);
-                }
-                
-            });
-        }
-        
-
+      });
     }
-    catch(err){
-        console.log(err);
-    }
-
-}
-module.exports ={ getCurrData, addOneData, logout, renderLogin, getPatientHistory, changePassword};
+  } catch (err) {
+    console.log(err);
+  }
+};
+module.exports = {
+  getCurrData,
+  addOneData,
+  logout,
+  renderLogin,
+  getPatientHistory,
+  renderChangePwd,
+  changePassword,
+};
--- router/patient.js
+++ router/patient.js
@@ -1,31 +1,32 @@
 const express = require("express");
 const patientController = require("../controller/patientController.js");
 const patientRouter = express.Router();
-const utility = require('./patientUtility.js')
-const passport = require('passport');
+const utility = require("./patientUtility.js");
+const passport = require("passport");
 const res = require("express/lib/response");
-
 
 // Main page which requires login to access
 
 // Login page (with failure message displayed upon login failure)
-patientRouter.get('/login', (req, res)=>res.render("../views/layouts/login.hbs")) // utility.unLoggedIn, 
+patientRouter.get("/login", (req, res) =>
+  res.render("../views/layouts/login.hbs")
+); // utility.unLoggedIn,
 // Handle login
-patientRouter.post('/login',
-    utility.unLoggedIn,
-    passport.authenticate("patient-login", {
-      //id should be here
-        failureReqirect: '/login',
-        failureFlash: true
-    }),
-    (req, res)=>{
-        console.log(req);
-        const user = req.user;
-        const link = "/patient/" + user._id.toString()
-        res.redirect(link);
-    }
-
-)
+patientRouter.post(
+  "/login",
+  utility.unLoggedIn,
+  passport.authenticate("patient-login", {
+    //id should be here
+    failureReqirect: "/login",
+    failureFlash: true,
+  }),
+  (req, res) => {
+    console.log(req);
+    const user = req.user;
+    const link = "/patient/" + user._id.toString();
+    res.redirect(link);
+  }
+);
 
 /*/ POST - user submits the signup form -- signup a new user
 customerRouter.post('/signup', passport.authenticate('local-signup', {
@@ -40,9 +41,8 @@ patientRouter.get("/:patient_id", patientController.getCurrData);
 //add current patient's data
 patientRouter.post("/:patient_id", patientController.addOneData);
 
-
 //change patient's password
-patientRouter.get("/:patient_id/password", (req, res, next) => {
+/* patientRouter.get("/:patient_id/password", (req, res, next) => {
 
     const form = '<h1>Change Password Page</h1><form method="post" action="">\
                     <br>Enter current password:<br><input type="password" name="currPassword">\
@@ -50,15 +50,19 @@ patientRouter.get("/:patient_id/password", (req, res, next) => {
                     <br><br><input type="submit" value="Submit"></form>';
 
     res.send(form);
-})
-patientRouter.post("/:patient_id/password", patientController.changePassword);
-// get current patient's history data that was entered
-patientRouter.get("/:patient_id/history_data", patientController.getPatientHistory);
+}) */
 
+patientRouter.get("/:patient_id/changePwd", patientController.renderChangePwd);
+
+patientRouter.post("/:patient_id/changePwd", patientController.changePassword);
+// get current patient's history data that was entered
+patientRouter.get(
+  "/:patient_id/history_data",
+  patientController.getPatientHistory
+);
 
 // When you visit http://localhost:3000/register, you will see "Register Page"
 
 // Handle logout
-patientRouter.post('/logout', utility.isLoggedIn, patientController.logout)
-module.exports = patientRouter
-
+patientRouter.post("/logout", utility.isLoggedIn, patientController.logout);
+module.exports = patientRouter;
