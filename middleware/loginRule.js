a733799e392c9522bcc591e522e26690404eaa08
juicyka <qzzhu1@student.unimelb.edu.au>
2022-05-18 22:23:59 +1000

    add login rule, remove unused about.js

---
+++ middleware/loginRule.js
@@ -0,0 +1,11 @@
+const { check, sanitizeBody } = require("express-validator");
+exports.form = [
+  // login
+  check("email")
+    .trim()
+    .notEmpty()
+    .withMessage("e-mail should not be empty")
+    .isEmail()
+    .withMessage("Please enter valid email"),
+  check("password").trim().notEmpty().withMessage("e-mail should not be empty"),
+];
