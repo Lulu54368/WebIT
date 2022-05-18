32186bb7d17383846abf8eb7d949fb5de59523a3
juicyka <qzzhu1@student.unimelb.edu.au>
2022-05-18 14:23:40 +1000

    render clinician login page, move old login page

--- router/clinician.js
+++ router/clinician.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const clinicianRouter = express.Router();
 const clinicianController = require("../controller/clinicianController");
-const utility = require("./clinicianUtility.js")
+const utility = require("./clinicianUtility.js");
 const passport = require("passport");
 // Handle login
 clinicianRouter.post(
@@ -19,56 +19,62 @@ clinicianRouter.post(
       const link = "/clinician/" + user._id.toString();
       res.redirect(link);
     }
-    
   }
 );
 
-clinicianRouter.get("/login", utility.unLoggedIn,
-    (req, res, next) => {
-
-      const form = '<h1>Login</h1><form method="post" action="">\
+clinicianRouter.get("/login", utility.unLoggedIn, (req, res, next) => {
+  const form =
+    '<h1>Login</h1><form method="post" action="">\
                   <br>Enter email<br><input type="email" name="email">\
                   <br>Enter password<br><input type="password" name="password">\
                   <br><br><input type="submit" value="Submit"></form>';
 
-    res.send(form);
+  //res.send(form);
+  res.render("../views/layouts/login");
+});
+clinicianRouter.get("/logout", utility.isLoggedIn, clinicianController.logout);
+
+clinicianRouter.post(
+  "/signup",
+  passport.authenticate("local-signup", {
+    successRedirect: "/clinician/login", // redirect to the homepage
+    failureRedirect: "/clinician/signup", // redirect to signup page
+    failureFlash: true, // should be true here
+  }),
+  (req, res, err) => {
+    {
+      console.log("hello");
+
+      const link = "/clinician/login";
+      res.redirect(link);
+    }
   }
 );
-clinicianRouter.get("/logout",utility.isLoggedIn, clinicianController.logout);
-clinicianRouter.post('/signup', passport.authenticate('local-signup', {
-  successRedirect : '/clinician/login', // redirect to the homepage
-  failureRedirect : '/clinician/signup', // redirect to signup page
-  failureFlash : true // should be true here 
-}),
-(req, res, err) => {
-  {
-    console.log("hello");
- 
-    const link = "/clinician/login"
-    res.redirect(link);
-  }
-  
-});
 
-clinicianRouter.get("/signup", utility.unLoggedIn,
-    (req, res, next) => {
-
-      const form = '<h1>Signup</h1><form method="post" action="">\
+clinicianRouter.get("/signup", utility.unLoggedIn, (req, res, next) => {
+  const form =
+    '<h1>Signup</h1><form method="post" action="">\
                   <br>Enter email<br><input type="email" name="email">\
                   <br>Enter firstname<br><input type="text" name="firstname">\
                   <br>Enter lastname<br><input type="text" name="lastname">\
                   <br>Enter password<br><input type="password" name="password">\
                   <br><br><input type="submit" value="Submit"></form>';
 
-    res.send(form);
-  }
-);
+  res.send(form);
+});
+
 //Get all patients' data of a specified clinician
-clinicianRouter.get("/:clinician_id",utility.isLoggedIn, clinicianController.getAllPatients);
+clinicianRouter.get(
+  "/:clinician_id",
+  utility.isLoggedIn,
+  clinicianController.getAllPatients
+);
 
-clinicianRouter.get("/:clinician_id/register",utility.isLoggedIn, clinicianController.renderRegister);
-
-
+clinicianRouter.get(
+  "/:clinician_id/register",
+  utility.isLoggedIn,
+  clinicianController.renderRegister
+);
 
 //have to replace every id here with clinician_id
 
@@ -76,59 +82,69 @@ clinicianRouter.get("/:clinician_id/register",utility.isLoggedIn, clinicianContr
 
 //add a patient
 clinicianRouter.post(
-  "/:clinician_id/register",utility.isLoggedIn,
+  "/:clinician_id/register",
+  utility.isLoggedIn,
   clinicianController.addOnePatient
 );
 
 //get all of patient's comment (id is clinincian id)
 clinicianRouter.get(
-  "/:clinician_id/comments",utility.isLoggedIn,
+  "/:clinician_id/comments",
+  utility.isLoggedIn,
   clinicianController.getAllComments
 );
 
 //get all of support sentence
 clinicianRouter.get(
-  "/:clinician_id/support",utility.isLoggedIn,
+  "/:clinician_id/support",
+  utility.isLoggedIn,
   clinicianController.getSupportSentence
 );
 
 //add support sentence
 clinicianRouter.post(
-  "/:clinician_id/support",utility.isLoggedIn,
+  "/:clinician_id/support",
+  utility.isLoggedIn,
   clinicianController.addSupportSentence
 );
 //get all of threshold
 clinicianRouter.get(
-  "/:clinician_id/threshold",utility.isLoggedIn,
+  "/:clinician_id/threshold",
+  utility.isLoggedIn,
   clinicianController.getAllThreshold
 );
 //change threshold of a specified patient
 clinicianRouter.post(
-  "/:clinician_id/threshold/",utility.isLoggedIn,
+  "/:clinician_id/threshold/",
+  utility.isLoggedIn,
   clinicianController.modifyThreshold
 );
 
 // get all clinical notes for a certain patient once clicked their name
 clinicianRouter.get(
-  "/:clinician_id/:patient_id/notes", utility.isLoggedIn,
+  "/:clinician_id/:patient_id/notes",
+  utility.isLoggedIn,
   clinicianController.getOnePatientAllNotes
 );
 
 // get all comments for a certain patient once clicked their name
 clinicianRouter.get(
-  "/:clinician_id/:patient_id/comments",utility.isLoggedIn,
+  "/:clinician_id/:patient_id/comments",
+  utility.isLoggedIn,
   clinicianController.getOnesComments
 );
 
 // Add a clinical note for a certain patient
 clinicianRouter.post(
-  "/:clinician_id/:patient_id/addnote", utility.isLoggedIn,
+  "/:clinician_id/:patient_id/addnote",
+  utility.isLoggedIn,
   clinicianController.addOnePatientNote
-)
+);
 
 //Add fields the patient need to input
 clinicianRouter.post(
-  "/:clinician_id/:patient_id/input",utility.isLoggedIn,
+  "/:clinician_id/:patient_id/input",
+  utility.isLoggedIn,
   clinicianController.modifyInput
 );
 
@@ -140,15 +156,16 @@ clinicianRouter.post(
 
 //add support sentence
 clinicianRouter.post(
-  "/:clinician_id/:patient_id/support",utility.isLoggedIn,
+  "/:clinician_id/:patient_id/support",
+  utility.isLoggedIn,
   clinicianController.addSupportSentence
 );
 
 //Get one patient's data of a specified clinician
 clinicianRouter.get(
-  "/:clinician_id/:patient_id",utility.isLoggedIn,
+  "/:clinician_id/:patient_id",
+  utility.isLoggedIn,
   clinicianController.getOnePatient
 );
-
 
 module.exports = clinicianRouter;
